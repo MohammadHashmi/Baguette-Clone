c9f1b76f8c292808b86369db46b8942c105ead34
Mohammad-h <784709@pdsb.net>
2023-11-15 22:30:32 -0700

    Got it looking mostly decent

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
   let navLinks = document.querySelector('.nav-links')
+  let centerText = document.querySelector('.center-text')
   navLinks.classList.toggle('show')
+  centerText.classList.toggle('show')
 }
